f8b99eed706a37c84fb132026bf0dab339ccc7ff
Wan Azlin <wannurulazlin@gmail.com>
2023-10-29 17:26:25 +0800

    third commit

---
+++ src/userinput.java
@@ -0,0 +1,11 @@
+/**
+ * userinput
+ */
+import java.util.Scanner;
+public class userinput {
+
+    public static void main(String args[]){
+        Scanner azlin = new Scanner(System.in);
+        System.out.println(azlin.nextLine());
+    }
+}
