8070a92545d5ec6f425d16af9fce1e85d2c0597f
Wan Azlin <wannurulazlin@gmail.com>
2023-11-03 16:23:42 +0800

    8th commit

---
+++ src/logicaloperator.java
@@ -0,0 +1,12 @@
+public class logicaloperator {
+    public static void main(String[] args) {
+        int boys=18;
+        int girls=17;
+
+        if (boys > 15 && girls > 19){
+            System.out.println("You can enter");
+        }else{
+            System.out.println("You are underage");
+        }
+    }
+}
