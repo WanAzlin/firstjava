92fbefe9193b3d15fb8f473c9328d64c484fabbf
Wan Azlin <wannurulazlin@gmail.com>
2023-11-03 16:03:44 +0800

    test

---
+++ src/ifstatement.java
@@ -0,0 +1,12 @@
+public class ifstatement {
+    public static void main(String[] args) {
+        int test = 90;
+
+        if (test > 80){
+            System.err.println("Get A");
+        }else{
+            System.err.println("Get B");
+        }
+    }
+    
+}
