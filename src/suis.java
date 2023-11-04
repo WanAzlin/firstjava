952cc74dd2b9ae5dd78cc6cc1d58982b0e6e25dd
Wan Azlin <wannurulazlin@gmail.com>
2023-11-04 13:45:57 +0800

    switch

---
+++ src/suis.java
@@ -0,0 +1,16 @@
+public class suis {
+    public static void main(String[] args) {
+        int age = 3;
+        switch(age){
+            case 1:
+                System.out.println("you can crawl");
+                break;
+            case 2:
+                System.out.println("you can talk");
+                break;
+            case 3:
+                System.out.println("you can walk");
+                break;
+        }
+    }
+}
