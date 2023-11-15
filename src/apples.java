9c049c6e2c596592cf00ac348a3752223215fb9d
Wan Azlin <wannurulazlin@gmail.com>
2023-11-15 09:29:16 +0800

    loop

---
+++ src/apples.java
@@ -0,0 +1,8 @@
+public class apples {
+    public static void main(String[] args) {
+        for (int counter =1; counter<=10; counter+=3){ //counter++
+            System.out.println(counter);
+        }
+    }
+}
+//loops take 2 or arguments/statements, 1st when to loop the start, loop the end, how much you want to increment
