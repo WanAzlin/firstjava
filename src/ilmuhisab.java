ac612d684b5db1c70d90dddc356ed6e4588d3eb4
Wan Azlin <wannurulazlin@gmail.com>
2023-11-16 14:03:30 +0800

    Math Class Method

---
+++ src/ilmuhisab.java
@@ -0,0 +1,11 @@
+public class ilmuhisab {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-26.7)); //add absolute value. takes one number and give value
+    }
+}
+//ceil - give a round up number for any double 8.9=9.0
+//floor - take first value for float number  7.0 = 7
+//max - take multiple parameter (Math.max(8.6,5.2)) = 8.6
+//min - take multiple parameter (Math.min(8.6,5.2)) = 5.2
+//pow - (Math.pow(5,3))
+//sqrt - (Math.sqrt(9))
