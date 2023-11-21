3292ff9d288b9dec4a8dbe34f2e6a9563ec115db
Wan Azlin <wannurulazlin@gmail.com>
2023-11-21 14:44:27 +0800

    array table

---
+++ src/array_table.java
@@ -0,0 +1,13 @@
+public class array_table {
+    public static void main(String[] args) {
+        System.out.println("Index\tValue");
+        int alin[] = {80,95,70,48,39};
+
+        for(int counter=0; counter<alin.length; counter++)
+        {
+            System.out.println(counter + "\t" + alin[counter]);
+        }
+
+
+    }
+}
