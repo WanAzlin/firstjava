9e76e0b05780b46aa33908102bff8c8a45dea2f9
Wan Azlin <wannurulazlin@gmail.com>
2023-11-21 14:26:12 +0800

    simple array

---
+++ src/array.java
@@ -0,0 +1,14 @@
+public class array {
+    public static void main(String[] args) {
+        int  alin[] = new int[10];
+
+        alin[0] =87;
+        alin[1] =53;
+        alin[2] =12;
+        alin[3] =56;
+       
+
+        System.out.println(alin[1]);
+
+    }
+}
