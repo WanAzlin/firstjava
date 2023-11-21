ef4bfa627f3f7b0d35791e478e0150867d701cf1
Wan Azlin <wannurulazlin@gmail.com>
2023-11-21 14:32:30 +0800

    initialize array

---
+++ src/arra_initializer.java
@@ -0,0 +1,15 @@
+public class arra_initializer {
+    public static void main(String[] args) {
+        
+        int zira[] = {1,2,3,4,5};
+
+        System.out.println(zira[2]);
+
+
+    }
+}
+
+
+
+      
+         
