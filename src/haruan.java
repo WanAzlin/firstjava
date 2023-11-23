0f7e8e4729307b03e584993c5d322f4c09dc5654
Wan Azlin <wannurulazlin@gmail.com>
2023-11-23 10:53:28 +0800

    enhanced loop

---
+++ src/haruan.java
@@ -0,0 +1,17 @@
+//enhanced loop
+
+public class haruan {
+    public static void main(String[] args) {
+        int azlin[] = {23,43,54,24,54}; //array initializer
+        int total = 0;
+
+        for (int x: azlin){  //identifiers
+            total +=x;
+
+        }
+
+        System.out.println(total);
+
+    }
+    
+} 
