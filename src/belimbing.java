1a2ba2aa81866e91f1faac93301412b0b8c4200c
Wan Azlin <wannurulazlin@gmail.com>
2023-11-28 09:25:58 +0800

    time military

---
+++ src/belimbing.java
@@ -0,0 +1,19 @@
+public class belimbing {
+    private int hour;
+    private int minute;
+    private int second;
+
+    public void setTime(int h, int m, int s){
+        hour = ((h >= 0 && h <24) ? h : 0);
+        minute = ((m >= 0 && m <60) ? m: 0);
+        second  = ((s >= 0 && s <60) ? s: 0);
+    }
+    public String toMilitary(){
+        return String.format("%02d:%02d:%02d", hour,minute,second);
+    }
+
+
+
+}
+//hour must be in between 0-24
+//minute must be in between 0-60
---
+++ src/starfruit.java
@@ -0,0 +1,15 @@
+public class starfruit {
+    public static void main(String[] args) {
+        belimbing belimbbingObject = new belimbing();
+        System.out.println(belimbbingObject.toMilitary());
+        belimbbingObject.setTime(13, 27, 6);
+        System.out.println(belimbbingObject.toMilitary());
+
+
+
+
+    }
+}
+
+
+//military time 
